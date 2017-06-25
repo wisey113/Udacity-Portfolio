1a8cc03b937b67de1d9c87125542835d8e8fadd5
Jamie Wise <wisey11@gmail.com>
2017-06-25 14:55:03 -0600

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          sizes: [{
+            
+            name: "large",
+            width: 1100,
+            suffix: "_x1",
+            quality: 30
+
+          },
+          {
+            name: "medium",
+            width: 750,
+            suffix: "_x1",
+            quality: 30
+          },
+          {
+            name: "medium",
+            width: 750,
+            suffix: "_x2",
+            quality: 75
+          },
+          {
+            name: "small",
+            width: 500,
+            suffix: "_x1",
+            quality: 30
+          },
+          {
+            name: "small",
+            width: 500,
+            suffix: "_x2",
+            quality: 75
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
